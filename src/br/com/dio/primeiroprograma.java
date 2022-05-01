06c44d4805ec38b76c32c954eecfe2ed62f3bf14
rosaliavale <rosaliavale@gmail.com>
2022-05-01 19:11:40 -0300

    First Commit

---
+++ src/br/com/dio/primeiroprograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import model.Gato;
+
+public class primeiroprograma {
+    public static void main(String[] args) {
+        /*System.out.println("hello world!");
+*/
+        Gato gato = new Gato ();
+        System.out.println(gato);
+    }
+}
